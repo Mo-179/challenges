bfcbf7686f0f931a3f549f2f25faf5a59dae1cb8
Mohammad Alkayem <m7ammad.7@gmail.com>
2024-04-16 12:43:42 +0200

    Add switch buttom

--- js-inputs-and-strings_switch-values/js/index.js
+++ js-inputs-and-strings_switch-values/js/index.js
@@ -14,13 +14,22 @@ Hint: the HTML entity for the switch button is: &updownarrow
 */
 
 const firstInput = document.querySelector('[data-js="first-input"]');
+const secondInput = document.querySelector('[data-js="second-input"]');
+const buttonSwitch = document.querySelector('[data-js="button-switch"]');
 const uppercaseButton = document.querySelector('[data-js="button-uppercase"]');
 const lowercaseButton = document.querySelector('[data-js="button-lowercase"]');
 
 uppercaseButton.addEventListener("click", () => {
   firstInput.value = firstInput.value.toUpperCase();
+  secondInput.value = secondInput.value.toUpperCase();
 });
 
 lowercaseButton.addEventListener("click", () => {
   firstInput.value = firstInput.value.toLowerCase();
+  secondInput.value = secondInput.value.toLowerCase();
+});
+switchInput.addEventListener("click", () => {
+  // const OrininalfirstInput = firstInput.value;
+  firstInput.value = secondInput.value;
+  secondInput.value = firstInput.value;
 });
