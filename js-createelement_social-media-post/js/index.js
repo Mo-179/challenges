329eab59373b805bf37d265ffecabc33e2b4818e
Mohammad Alkayem <m7ammad.7@gmail.com>
2024-04-17 13:07:00 +0200

    add a post

--- js-createelement_social-media-post/js/index.js
+++ js-createelement_social-media-post/js/index.js
@@ -10,3 +10,26 @@ likeButton.addEventListener("click", handleLikeButtonClick);
 
 // Exercise:
 // Use document.createElement() and append another social media post to the body.
+
+const addPost = document.createElement("section");
+addPost.classList.add("post");
+
+const addText = document.createElement("p");
+addText.classList.add("post__content");
+addText.textContent =
+  "Lorem ipsum dolor, sit amet consectetur adipisicing elit.";
+
+const addFooter = document.createElement("footer");
+addFooter.classList.add("post__footer");
+
+const addSpan = document.createElement("span");
+addSpan.classList.add("post__username");
+addSpan.textContent = "@username";
+
+const addLikeButton = document.createElement("button");
+addLikeButton.classList.add("post__button");
+addLikeButton.textContent = "♥ Like";
+addLikeButton.addEventListener("click", handleLikeButtonClick);
+addFooter.append(addSpan, addLikeButton);
+addPost.append(addText, addFooter);
+document.body.append(addPost);
