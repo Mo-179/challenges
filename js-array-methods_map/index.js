18702ec8ad289d36c75b66d567150373fbd541f2
Mohammad Alkayem <m7ammad.7@gmail.com>
2024-04-22 16:57:12 +0200

    Map

--- js-array-methods_map/index.js
+++ js-array-methods_map/index.js
@@ -24,10 +24,23 @@ const cards = [
   },
 ];
 
-const lowerCaseAnswers = null; // ['as often as you like.', ...]
+const lowerCaseAnswers = cards.map((card) => {
+  return card.answer.toLowerCase();
+}); // ['as often as you like.', ...]
 
-const questionsAndAnswersTogether = null; // ["How often can I use <header>? - As often as you like.", ...]
+const questionsAndAnswersTogether = cards.map((card) => {
+  return card.question + "-" + card.answer;
+}); // ["How often can I use <header>? - As often as you like.", ...]
 
-const questionAndAnswer = null; // [{ question: 'How often can I use <header>?', answer: 'As often as you like.'}, {...}]
+const questionAndAnswer = cards.map((card) => {
+  const qAObject = {};
+  qAObject.question = card.question;
+  qAObject.answer = card.answer;
+
+  return qAObject;
+
+  // return cards.entries(card.question), cards.entries(card.answer);??????????
+}); // [{ question: 'How often can I use <header>?', answer: 'As often as you like.'}, {...}]
+console.log(questionAndAnswer);
 
 export { lowerCaseAnswers, questionsAndAnswersTogether, questionAndAnswer };
