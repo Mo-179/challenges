5f3b6f460be9680fa311218057d80fd2a1162e2a
Mohammad Alkayem <m7ammad.7@gmail.com>
2024-05-03 16:31:00 +0200

    add react component with attributes

--- react-basics_attributes/src/App.jsx
+++ react-basics_attributes/src/App.jsx
@@ -1,5 +1,18 @@
 import "./App.css";
 
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return ArticleComponent();
+}
+
+function ArticleComponent() {
+  return (
+    <article className="article">
+      <h2 className="article__title">some title</h2>
+      <label htmlFor="input">Lable</label>
+      <input id="input"></input>
+      <a className="article_link" href="https://de.wikipedia.org/wiki/Pizza">
+        hungry?{" "}
+      </a>
+    </article>
+  );
 }
