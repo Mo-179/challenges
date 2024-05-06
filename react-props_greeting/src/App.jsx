e48984a66f20212c17eb8e96f0898ad8a9bf9088
Mohammad Alkayem <m7ammad.7@gmail.com>
2024-05-06 11:08:29 +0200

    add greeting props

--- react-props_greeting/src/App.jsx
+++ react-props_greeting/src/App.jsx
@@ -1,5 +1,13 @@
 import "./App.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return (
+    <h1>
+      <Greeting name={"user"} />
+    </h1>
+  );
+}
+
+function Greeting(props) {
+  return <div>Hello {props.name}</div>;
 }
